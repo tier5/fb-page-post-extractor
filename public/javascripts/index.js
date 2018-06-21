3d347e0b9eee9b6759a7cac94148576dc3f041e0
jon vaughn <jon@thinkvaughn.com>
2018-06-22 00:48:20 +0530

    Code for multiple page url support

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,27 +6,61 @@
         var loader = document.querySelector('.loader')
 
         getAllPostsButton.addEventListener('click', function () {
-            var params = {
-                page_access_token: pageAccessTokenInput.value,
-                fb_page_url: fbPageURLInput.value
-            }
+            
             loader.style.display = 'block'
 
-            axios.get('/posts', {
-                params: params
-            }).then(function (res) {
-                var csvContent = "data:text/csv;charset=utf-8," + res.data
-                var encodedUri = encodeURI(csvContent)
+            if(pageAccessTokenInput.value !== "" && pageAccessTokenInput.value !== null){
+                
+                var values = []
 
-                var downloadLink = document.querySelector('#download-csv')
-                downloadLink.setAttribute("href", encodedUri)
-                downloadLink.setAttribute("download", "posts.csv")
-                downloadLink.style.display = "inline"
-                downloadLink.style.padding = "10px"
-                downloadLink.style.border = "1px solid #218838"
+                $('.fb-page-url').each(function () {
+                    if (this.value != '')
+                        values.push(this.value)
+                })
 
-                loader.style.display = 'none'
-            })
+                var params = {
+                    page_access_token: pageAccessTokenInput.value,
+                    fb_page_url: values
+                }
+
+                $.ajax({
+                    type: 'POST',
+                    url: "/posts",
+                    data: params,
+                    success: function(res) {
+                        var csvContent = "data:text/csv;charset=utf-8," + res
+                        var encodedUri = encodeURI(csvContent)
+
+                        var downloadLink = document.querySelector('#download-csv')
+                        downloadLink.setAttribute("href", encodedUri)
+                        downloadLink.setAttribute("download", "posts.csv")
+                        downloadLink.style.display = "inline"
+                        downloadLink.style.padding = "10px"
+                        downloadLink.style.border = "1px solid #218838"
+
+                        loader.style.display = 'none'
+                    }
+                })
+            }
         })
     })
 })(document, window)
+
+$(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+    
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" name="fb-page-url[]" class="form-control fb-page-url" placeholder="Facebook Page URL" autocomplete="off" required><a href="#" class="remove_field">Remove</a></br></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,114 @@ router.get('/dashboard', function(req, res, next) {
   res.render('pages/index');
 });
 
-router.get('/posts',(request,response,next) => {
+router.post('/posts', function (request, response) {
+
+    const pageAccessToken = request.body.page_access_token
+    const fbPageURLs = request.body.fb_page_url
+
+    const graphAPIBase = process.env.GRAPH_API_HOST + process.env.GRAPH_API_VERSION
+
+    for(var i=0; i < fbPageURLs.length; i++) {
+
+        console.log(fbPageURLs[i])
+
+        const getAllPostsFromFBPage = (pageId, nextPageURL, posts) => {
+            const graphAPIURLForPostsOfAPage = nextPageURL ? nextPageURL : graphAPIBase + '/'+ pageId +'/posts'
+            const fields = 'message,link,permalink_url,created_time,type,name,id,comments.limit(0).summary(true),shares,likes.limit(0).summary(true),reactions.limit(0).summary(true)'
+            const params = nextPageURL ? {} : {
+                access_token: pageAccessToken,
+                fields: fields,
+                limit: 100
+            }
+
+            axios.get(graphAPIURLForPostsOfAPage, {
+                params: params
+            }).then(res => {
+                if (res.data) {
+                    if (res.data.paging) {
+                        if (res.data.paging.next) {
+                            getAllPostsFromFBPage(pageId, decodeURIComponent(res.data.paging.next), posts.concat(res.data.data))
+                        } else {
+                            posts.concat(res.data.data)
+                            const options = {
+                                delimiter: {
+                                    wrap: '"',
+                                    field: ',',
+                                    array: ';',
+                                    eol: '\n'
+                                },
+                                prependHeader: true,
+                                sortHeader: false,
+                                trimHeaderValues: true,
+                                trimFieldValues:  true,
+                                keys: [
+                                    'message',
+                                    'link',
+                                    'permalink_url',
+                                    'created_time',
+                                    'type',
+                                    'id',
+                                    'comments.data',
+                                    'comments.summary.order',
+                                    'comments.summary.total_count',
+                                    'comments.summary.can_comment',
+                                    'likes.data',
+                                    'likes.summary.total_count',
+                                    'likes.summary.can_like',
+                                    'likes.summary.has_liked',
+                                    'reactions.data',
+                                    'reactions.summary.total_count',
+                                    'reactions.summary.viewer_reaction'
+                                ]
+                            };
+                            converter.json2csv(posts, (err, csv) => {
+                                if (err) {
+                                    console.log('Error converting JSON to CSV: ', err)
+                                    response.status(500).send(err)
+                                } else {
+                                    console.log('Total posts fetched: ', posts.length)
+                                    response.send(csv)
+                                }
+                            }, options)
+                        }
+                    }
+                }
+            })
+            .catch(err => {
+                console.log('Posts per page fetching error', err)
+                response.status(500).send(err.message)
+            })
+        }
+
+        const fbPageURLSegments = fbPageURLs[i].split('/')
+        const pageSlug = fbPageURLSegments[3] == 'pg' ? fbPageURLSegments[4] : fbPageURLSegments[3]
+        let pageSlugNumber = pageSlug.match(/[\d]*$/g)
+        /** Check if a page slug already has the number id or not,
+         * if has then get the post else get the number id from Graph API
+         */
+        if (pageSlugNumber[0]) {
+            pageSlugNumber = pageSlugNumber.join("")
+            getAllPostsFromFBPage(pageSlugNumber, null, [])
+        } else {
+            const graphAPIURLForPageNumberID = graphAPIBase + '/' + pageSlug
+            const params = {
+                access_token: pageAccessToken
+            }
+
+            axios.get(graphAPIURLForPageNumberID, {
+                params: params
+            }).then(res => {
+                getAllPostsFromFBPage(res.data.id, null, [])
+            })
+            .catch(err => {
+                console.log('Page number ID fetching error: ', err)
+                response.status(500).send(err.message)
+            })
+        }
+    }
+});
+
+/*router.get('/posts',(request,response,next) => {
   const urlParts = url.parse(request.url, true)
     const pageAccessToken = urlParts.query.page_access_token
     const fbPageURL = urlParts.query.fb_page_url
@@ -90,9 +197,9 @@ router.get('/posts',(request,response,next) => {
     const fbPageURLSegments = fbPageURL.split('/')
     const pageSlug = fbPageURLSegments[3] == 'pg' ? fbPageURLSegments[4] : fbPageURLSegments[3]
     let pageSlugNumber = pageSlug.match(/[\d]*$/g)
-    /** Check if a page slug already has the number id or not,
-     * if has then get the post else get the number id from Graph API
-     */
+    /// Check if a page slug already has the number id or not,
+      if has then get the post else get the number id from Graph API
+    ///
     if (pageSlugNumber[0]) {
         pageSlugNumber = pageSlugNumber.join("")
         getAllPostsFromFBPage(pageSlugNumber, null, [])
@@ -112,5 +219,6 @@ router.get('/posts',(request,response,next) => {
             response.status(500).send(err.message)
         })
     }
-});
+});*/
+
 module.exports = router;
