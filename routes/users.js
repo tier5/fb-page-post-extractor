079a9677a4704bcdcde9858103163d1c1441eab3
Jon Vaughn <jon@thinkvaughn.com>
2018-06-27 22:10:48 +0530

    hooks to delete,suspend and unsuspend account done

--- routes/users.js
+++ routes/users.js
@@ -51,9 +51,105 @@ router.post('/login', async (req,res,next) => {
     console.log(error);
     res.status(500).send({message : "Something went wrong!", status: false})
   }
+});
 
+/**
+ * Route to delete user from the application
+ * @param {object} request
+ * @param {object} response
+ * @returns response
+ */
+router.post("/delete",function(req,res,next){
+  let email = req.body.email || req.params.email;
+  var token = req.body.token ;
+  let testTokenNumber  =  '0987654321';
+  // checking token 
+  if(!token || token != testTokenNumber){
+      return res.status(200).send({status :false , message : 'Token mismatch!'})
+  };
+  // checking email
+  if (!email){ return res.status(200).send({message : 'email is required!' , status : false})};
+  Users.findOneAndRemove({
+    email : email
+  }).then(removed => {
+    console.log(removed);
+    if (!removed) { return res.status(200).send({status : false , message : 'Account not found!'})}
+    return res.status(200).send({ status :true , message : 'Account deleted!'});
+  }).catch(error=>{
+    console.log(error);
+    return res.status(200).send({message : 'Server Internal Error!', status:false});
+  })
   
-})
+});
+
+
+// route to suspend and unsuspend accout
+router.post("/suspend",suspendUserFromHook);
+router.post("/unsuspend",unsuspendUserFromHook);
+
+/**
+ * Function to unsuspend an account
+ * @param {object} req 
+ * @param {object} res 
+ */
+function unsuspendUserFromHook (req, res) {
+  var token = req.body.token ;
+  let testTokenNumber  =  '0987654321';
+  var email = req.body.email;
+  // checking token 
+  if(!token || token != testTokenNumber){
+      return res.status(200).send({status :false , message : 'Token mismatch!'})
+  };
+  // checking email
+  if (!email){ return res.status(200).send({message : 'email is required!' , status : false})};
+  let ckeckSuspendedEmail = email + '_suspend';
+  Users.findOneAndUpdate({
+      email : ckeckSuspendedEmail
+  },
+  {
+      $set :{
+          email : email
+      }
+  }).then(updated =>{
+      if (!updated) { return res.status(200).send({ status : false , message : 'Account not found!'})}
+      return res.status(200).send({status : true , message : 'Account unsuspended'});
+  }).catch(error=>{
+      return res.status(200).send({message : 'Server Internal Error!', status:false});
+  })
+}
+
+/**
+ * Function to suspend an account
+ * @param {object} req 
+ * @param {object} res 
+ */
+function suspendUserFromHook(req, res) {
+  var token = req.body.token ;
+  let testTokenNumber  =  '0987654321';
+  var email = req.body.email;
+  // checking token 
+  if(!token || token != testTokenNumber){
+      return res.status(400).send({http_code : 400, status :'error' , message : 'Token mismatch!'})
+  };
+  // checking email
+  if (!email){ return res.status(400).send({message : 'email is required!' , status : false})};
+  
+  Users.findOneAndUpdate({
+      email : email
+  },
+  {
+      $set :{
+          email : email + "_suspend"
+      }
+  }).then(updated =>{
+      if (!updated) { return res.status(200).send({status : false , message : 'Account not found!'})}
+      return res.status(200).send({ status :true , message : 'Account suspended'});
+  }).catch(error=>{
+      return res.status(200).send({message : 'Server Internal Error!', status:false});
+  })
+}
+
+
 
 
 module.exports = router;
